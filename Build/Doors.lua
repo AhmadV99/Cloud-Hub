b89db0027a29a6ac623841472861aaef7ccc5888
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2024-09-01 23:12:40 +0400

    Update Doors.lua

--- Build/Doors.lua
+++ Build/Doors.lua
@@ -105,10 +105,6 @@ local _main = Window:MakeTab({Name = "Main", Icon = "Home"}) do
       end
     end
   end)
-  _main:AddSection({"Safe Mode"})
-  Funcs:Slider(_main, "Check Health To Return Safe", 0, 100, 60, 1)
-  Funcs:Slider(_main, "Check Health To Return Back", 0, 100, 90, 1)
-  Funcs:Toggle(_main, "Auto Safe Mode", false)
   _main:AddSection({"Other"})
   Funcs:Toggle(_main, "Anti-Screech", false)
   Funcs:Toggle(_main, "Auto Get Win Heartbeat", false)
